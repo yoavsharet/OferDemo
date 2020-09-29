b2069b0e7cb915510ed7dd9efa0c8396e08adf1a
yoavsharet <yoavsharet@gmail.com>
2020-09-29 16:46:58 +0300

    created ui demo

---
+++ FE/demo-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Demo from './components/demoComponent/demoComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ofer Demo</h1>
+      <Demo></Demo>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ FE/demo-app/src/components/demoComponent/demoComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Demo = () => {
+
+    return (
+        <div>
+            <h3>This is my demo</h3>
+            <h4>this is data from the BE</h4>
+        </div>
+    );
+  }
+  
+  export default Demo;
